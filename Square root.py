e92f00cddd2a8c68dd9ddfb7d562df7ab8aa17f3
Priyanka V <89244771+Priyankavad@users.noreply.github.com>
2021-09-17 09:03:44 +0530

    Create Square root.py

---
+++ Square root.py
@@ -0,0 +1,8 @@
+# Python program to find square root of the number
+hp = float(input('Enter the number: '))
+ 
+# calculate square root
+sqrt = hp ** 0.5
+ 
+# display result
+print('Square root of %0.2f is %0.2f '%(hp, sqrt))
