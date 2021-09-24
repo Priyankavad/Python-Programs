df85da937cdb73a20f3e8faecdd983734026171c
Priyanka V <89244771+Priyankavad@users.noreply.github.com>
2021-09-24 17:42:35 +0530

    Create arithmetic operators.py

---
+++ arithmetic operators.py
@@ -0,0 +1,21 @@
+# Store input numbers:  
+n1 = input('Enter first number: ')  
+n2 = input('Enter second number: ')  
+  
+# Add two numbers  
+add = float(n1) + float(n2)  
+# Subtract two numbers  
+sub = float(n1) - float(n2)  
+# Multiply two numbers  
+mult = float(n1) * float(n2)  
+#Divide two numbers  
+div = float(n1) / float(n2)  
+# Display the sum  
+print('The sum of {0} and {1} is {2}'.format(n1, n2, add))  
+  
+# Display the subtraction  
+print('The subtraction of {0} and {1} is {2}'.format(n1, n2, sub))  
+# Display the multiplication  
+print('The multiplication of {0} and {1} is {2}'.format(n1, n2, mul))  
+# Display the division  
+print('The division of {0} and {1} is {2}'.format(n1, n2, div)) 
