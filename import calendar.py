2ce0b8db0e0f7f14605c2bf9e863738e0b9e4345
Priyanka V <89244771+Priyankavad@users.noreply.github.com>
2021-09-24 17:38:37 +0530

    Create import calendar.py

---
+++ import calendar.py
@@ -0,0 +1,7 @@
+import calendar  
+# Enter the month and year  
+year = int(input("Enter year: "))  
+month = int(input("Enter month: "))  
+  
+# display the calendar  
+print(calendar.month(year,month))  
